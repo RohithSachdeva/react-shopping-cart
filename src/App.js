759bcb8c82b998223c60deaf99c14edbf12d09d2
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-05-14 21:26:08 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -12,23 +14,36 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to t
+		setCart([...cart, item]);
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={cart}>
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route exact path="/">
+						<Products />
+					</Route>
+
+					<Route path="/cart">
+						<ShoppingCart cart={cart} />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
 
 export default App;
+
+
+// Wrap all of your components/routes in App.js inside of ProductContext.Provider component.
+
+// Next pass a value prop to your Provider.
+
+// In the value prop we'll pass in the products state, and an addItem function that will allow us to add books to the cart.
+
+// <ProductContext.Provider value={{ products, addItem }}>
